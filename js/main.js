8b9959b79c58cdc226a9f6d45c532235526db176
TheWalrusman <Kyledcusack@gmail.com>
2020-04-16 15:49:42 -0600

    added correct Regex for correct formula

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let dispalyArray = [];
+let validNumericExpressionRegex = /^(([0-9]{1,}(\.?[0-9]{1,})?)([\/\*\-\+])*)+\=$/gm;
+let doubleDecimalCheckRegex = /(\.\.)/gm;
+let invalidFloatRegex = /([0-9]+\.[0-9]*\.)/gm;
+let numberRegex = /[0-9]/;
+let decimalRegex = /\./;
+
+function checkProperKeyInput() {
+  if (dispalyArray[dispalyArray.length-1].search(numberRegex) >= 0)
+  {
+
+  }else if(dispalyArray[dispalyArray.length-1].search(decimalRegex) >= 0)
+  {
+
+  }
+
+
+
+
+}
+
+function decimalKeyBlocker() {
+
+
+
+}
+
